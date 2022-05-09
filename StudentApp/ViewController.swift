547b6f9322afa52ff8888ae85d0c993f9eb519b2
Shimon Cohen <shimoco7@cs.colman.ac.il>
2022-05-09 19:11:55 +0300

    finished with custom segues

--- StudentApp/ViewController.swift
+++ StudentApp/ViewController.swift
@@ -9,12 +9,17 @@ import UIKit
 
 class ViewController: UIViewController , MySegueProtocol {
     
-    
     @IBAction func aboutBtn(_ sender: UIButton) {
+        removeSubViews()
+        performSegue(withIdentifier: "aboutSegue", sender: self)
     }
     @IBAction func addBtn(_ sender: UIButton) {
+        removeSubViews()
+        performSegue(withIdentifier: "addStudentSegue", sender: self)
     }
     @IBAction func listBtn(_ sender: UIButton) {
+        removeSubViews()
+        performSegue(withIdentifier: "studentListSegue", sender: self)
     }
     func getViewContainer(identifier: String) -> UIView {
         return containerView
@@ -23,9 +28,15 @@ class ViewController: UIViewController , MySegueProtocol {
     @IBOutlet weak var containerView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        performSegue(withIdentifier: "studentListSegue", sender: self)
         // Do any additional setup after loading the view.
     }
-
+    
+    func removeSubViews(){
+        for v in containerView.subviews{
+            v.removeFromSuperview()
+        }
+    }
 
 }
 
